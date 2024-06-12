2be7cf38d14ded92c84f2246e5a23b670cf73621
Oke <olorundadanny@gmail.com>
2024-06-12 20:24:16 +0100

    first commit

---
+++ src/UserInfo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UserInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("What is your title?");
+        Scanner InputVariableName = new Scanner (System.in);
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("What is your first name?");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("What is your last name?");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("Enter your email address");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("Enter your phone number");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("What is the name of your Country?");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("Enter your date of birth");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("Insert your place of birth");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("What is your current location?");
+        System.out.println("You entered " + InputVariableName.nextLine());
+        System.out.println("Insert your gender");
+        System.out.println("You entered " + InputVariableName.nextLine());
+    }
+}
